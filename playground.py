7b62656292cab4f3ab49c3916d7018cdd35eb140
ajit patil <ajitpatil.10@outlook.com>
2023-01-12 08:01:34 +0530

    Day-9 Code, needs some work still

---
+++ playground.py
@@ -0,0 +1,14 @@
+# import random
+
+# cards = [11, 2, 3, 4, 5, 6, 10, 10, 10]
+
+# user_cards = random.choices(cards, k=2)
+
+# print(user_cards)
+
+# if 11 in user_cards:
+#     print("yes there is an Ace")
+#     for n in range(0, len(user_cards)):
+#         if user_cards[n] == 11:
+#             print(f"Ace index is - {n}, and here it is {user_cards[n]}")
+            

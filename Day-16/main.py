5b1327c51ad96e4583612266326e8fc14aa99c4d
ajit patil <ajitpatil.10@outlook.com>
2023-01-19 16:48:12 +0530

    Going through the Day-16

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,45 +1,6 @@
-from turtle import Turtle, Screen
-from random import randint
+import colorgram
+from turtle import Turtle,,,,,,,,,, Screen
 
-tim = Turtle()
-# tim.shape("arrow")
-tim.color("dark magenta")
-
-# for _ in range(10):
-#     tim.forward(4)
-#     tim.penup()
-#     tim.forward(4)
-#     tim.pendown()
-
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
-
-for _ in range(5):
-    tim.forward(100)
-    tim.right(72)
-
-for _ in range(6):
-    tim.forward(100)
-    tim.right(360/6)
-
-for _ in range(7):
-    tim.forward(100)
-    tim.right(360/7)
-
-for _ in range(8):
-    tim.forward(100)
-    tim.right(360/8)
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
-screen.exitonclick()
+colors = colorgram.extract('image.jpg', 10)
+first_color = colors[0]
 

4b925a1531b45c2956987d870462dca9453a3bdd
ajit patil <ajitpatil.10@outlook.com>
2023-02-17 21:45:02 +0530

    Completed first 3 problems in SnakeGame

--- Day-18/main.py
+++ Day-18/main.py
@@ -2,3 +2,26 @@
 # Create a sanke
 # Move a snake 
 # Control Snake
+
+
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+snake.create_snake()
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
